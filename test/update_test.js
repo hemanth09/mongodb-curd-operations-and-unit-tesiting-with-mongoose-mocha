68c45e27017db347a9622a99dfbdf6a4b2e8fa68
hemanth reddy <hemanthreddyb@hotmail.com>
2020-01-15 23:00:04 +0000

    add validations

--- test/update_test.js
+++ test/update_test.js
@@ -15,7 +15,7 @@ describe("Updating records", () => {
   let joe;
 
   beforeEach(done => {
-    joe = new User({ name: "joe" });
+    joe = new User({ name: "joe", postCount: 0 });
     joe.save().then(() => done());
   });
 
@@ -38,5 +38,14 @@ describe("Updating records", () => {
 
   it("A model class can find a record and update it", done => {
     assertName(User.findByIdAndUpdate(joe._id, { name: "Alex" }), done);
+  });
+
+  it("A user can have their post count incremented by 1", done => {
+    User.update({ name: "joe" }, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: "joe" }))
+      .then(user => {
+        assert(user.postCount === 1);
+        done();
+      });
   });
 });
